2f9452abb2bc027303d53130418302caefd80a60
Serena Liu <72056768+Serena-LiuWx@users.noreply.github.com>
2023-05-15 02:42:08 +0800

    Update mixtures.py

--- flan/mixtures.py
+++ flan/mixtures.py
@@ -16,16 +16,16 @@
 
 import functools
 import seqio
-
-from flan import few_shot
-from flan import task_splits
-from flan import tasks  # pylint: disable=unused-import
-from flan import templates  # pylint: disable=unused-import
+from collections import defaultdict
+import few_shot
+import task_splits
+import tasks  # pylint: disable=unused-import
+import templates  # pylint: disable=unused-import
 
 mixing_rate_3k = functools.partial(seqio.mixing_rate_num_examples, maximum=3000)
 
 all_splits = []
-
+'''
 for shot_config in few_shot.ShotConfig:
   # Add inter cluster splits.
   all_splits += task_splits.generate_inter_cluster_splits(
@@ -50,7 +50,9 @@ for shot_config in few_shot.ShotConfig:
 
   # Add diversity ablation on number of clusters in finetuning.
   all_splits += task_splits.generate_inter_ablation(shot_config=shot_config)
-
+'''
+shot_config = few_shot.ShotConfig.ZERO
+all_splits = task_splits.generate_test_cluster_splits()
 for split in all_splits:
   seqio.MixtureRegistry.add(
       name=split.train_mixture_name,
@@ -60,3 +62,36 @@ for split in all_splits:
       name=split.eval_mixture_name,
       tasks=split.test_tasks,
       default_rate=seqio.mixing_rate_num_examples)
+
+# to get all the data from splits
+for split in all_splits:
+    selected_mixture = seqio.get_mixture_or_task(split.eval_mixture_name)
+
+    INPUT_SEQ_LEN = 2056
+    TARGET_SEQ_LEN = 512
+
+    dataset = selected_mixture.get_dataset(
+        sequence_length={"inputs": INPUT_SEQ_LEN, "targets": TARGET_SEQ_LEN},
+        num_epochs=1,
+        shuffle=True,
+        copy_pretokenized=True,
+        # The passthrough features let you track the source/task/template metadata for the example
+        passthrough_features=["_template_idx", "_task_source", "_task_name", "_template", "_template_type"]
+    )
+
+    # To read out the data you can do something like this:
+    save_data = []
+    source_counter = defaultdict(lambda: 0)
+    NUM_SAMPLES = 100
+    # If you would like to take min(1 epoch, NUM_SAMPLES) then use dataset.take(NUM_SAMPLES)
+    # Or if you would like to gather a full epoch, simply `enumerate(dataset)` until completion.
+    for i, ex in enumerate(dataset.take(NUM_SAMPLES)):
+        source_counter[ex["_task_source"].numpy()] += 1
+        save_data.append((ex["inputs_pretokenized"].numpy().decode(),
+                        ex["targets_pretokenized"].numpy().decode()))
+
+    print(f"Data Submixture Counts: {source_counter}")
+
+    print(save_data)
+    # with open('output_test.txt', 'w') as file:
+    #     file.write(str(save_data))
